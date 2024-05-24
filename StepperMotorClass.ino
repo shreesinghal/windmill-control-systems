8e1d86cac096a3a5c06611d518c7ee7ed63905fd
ritika-gurjar <61918369+ritika-gurjar@users.noreply.github.com>
2024-05-24 17:35:35 -0400

    Add files via upload

--- StepperMotorClass.ino
+++ StepperMotorClass.ino
@@ -4,6 +4,7 @@ class StepperMotor {
   const int dirPin = 2;
   const int stepPin = 3;
   const int stepsPerRevolution = 200;
+  const double anglePerStep = 1.8;
 
   StepperMotor() {
     pinMode(stepPin, OUTPUT);
@@ -14,18 +15,51 @@ class StepperMotor {
 
   
 
+  // negative 
   public void spinClockwise() {
     digitalWrite(dirPin, HIGH);
   }
   
+  // positive
   public void spinCounterclockwise() {
     digitalWrite(dirPin, LOW);
   }
 
   // resets the stepper to 0
   public void resetStepper() {
-    
+    // send it the negative of angle
+
   }
+
+  // Set to angle
+  // Sets to the best approximation for angle
+  // Divides the angle by 1.8
+  public void setAngle(double newAngle) {
+    if (this.angle != newAngle) {
+      // sets it counter 
+      if (newAngle > this.angle) {
+        this.spinCounterclockwise();
+      }
+      else {
+        this.spinClockwise();
+      }
+
+      for(int x = 0; x < this.angleToSteps(newAngle); x++)
+      {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(3000);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(3000);
+      }
+      this.angle = newAngle;
+
+    }
+  }
+
+  private int angleToSteps(double angle) {
+    return angle / anglePerStep;
+  }
+
   
 }
 
@@ -34,10 +68,17 @@ class StepperMotor {
 
 void setup() {
   // put your setup code here, to run once:
-
+  StepperMotor s = new StepperMotor();
+  float degree;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.begin(9600);
+  Serial.println("Enter in a degree: ");
+  if (Serial.available() > 0) {
+    degree = Serial.parseFloat();
+    s.setAngle(degree);
+  }
 
 }
