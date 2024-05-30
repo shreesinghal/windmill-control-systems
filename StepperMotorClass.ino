0937c7ccbd388547fdd9acc29faf22c455ee90c4
ritika-gurjar <61918369+ritika-gurjar@users.noreply.github.com>
2024-05-30 19:07:00 -0400

    Add files via upload

--- StepperMotorClass.ino
+++ StepperMotorClass.ino
@@ -1,84 +1,93 @@
-class StepperMotor {
-  
-  double angle;
-  const int dirPin = 2;
-  const int stepPin = 3;
-  const int stepsPerRevolution = 200;
-  const double anglePerStep = 1.8;
 
+
+class StepperMotor {
+  private:
+    double angle;
+    const int dirPin = 2;
+    const int stepPin = 3;
+    const int stepsPerRevolution = 200;
+    const double anglePerStep = 1.8;
+
+  public: 
   StepperMotor() {
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
-    this.angle = 0;
-    
+    this->angle = 0;
   }
 
-  
-
-  // negative 
-  public void spinClockwise() {
-    digitalWrite(dirPin, HIGH);
-  }
-  
-  // positive
-  public void spinCounterclockwise() {
-    digitalWrite(dirPin, LOW);
-  }
-
-  // resets the stepper to 0
-  public void resetStepper() {
-    // send it the negative of angle
-
-  }
-
-  // Set to angle
-  // Sets to the best approximation for angle
-  // Divides the angle by 1.8
-  public void setAngle(double newAngle) {
-    if (this.angle != newAngle) {
-      // sets it counter 
-      if (newAngle > this.angle) {
-        this.spinCounterclockwise();
-      }
-      else {
-        this.spinClockwise();
-      }
-
-      for(int x = 0; x < this.angleToSteps(newAngle); x++)
-      {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(3000);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(3000);
-      }
-      this.angle = newAngle;
-
+    // negative 
+    void spinClockwise() {
+      digitalWrite(dirPin, HIGH);
     }
-  }
+    
+    // positive
+    void spinCounterclockwise() {
+      digitalWrite(dirPin, LOW);
+    }
 
-  private int angleToSteps(double angle) {
-    return angle / anglePerStep;
-  }
+    // resets the stepper to 0
+    void resetStepper() {
+      // send it the negative of angle
+      this->setAngle(0);
+    }
+
+    // Set to angle
+    // Sets to the best approximation for angle
+    // Divides the angle by 1.8
+    void setAngle(double newAngle) {
+      if (newAngle >= 90) {
+        Serial.println("Degree value out of bounds");
+      }
+      else if (this->angle != newAngle) {
+
+        // sets it counter 
+        if (newAngle > this->angle) {
+          this->spinCounterclockwise();
+        }
+        else {
+          this->spinClockwise();
+        }
+
+        for(int x = 0; x < abs(this->angleToSteps(newAngle - this->angle)); x++)
+        {
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(3000);
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(3000);
+        }
+        this->angle = newAngle;
+
+      }
+    }
+
+  private:
+    int angleToSteps(double angle) {
+      return angle / anglePerStep;
+    }
 
   
-}
+};
 
 //change in wind speed from one state to another surpasses a certain threshold
+
+StepperMotor *s;
 
 
 void setup() {
   // put your setup code here, to run once:
-  StepperMotor s = new StepperMotor();
-  float degree;
+  s = new StepperMotor();
+  Serial.begin(9600);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.begin(9600);
+
+  float degree;
   Serial.println("Enter in a degree: ");
   if (Serial.available() > 0) {
     degree = Serial.parseFloat();
-    s.setAngle(degree);
+    s->setAngle(degree);
   }
 
 }
