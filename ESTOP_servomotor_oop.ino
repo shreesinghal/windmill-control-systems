6e3e55fc407b0276c1d2730b2752bab6f15169b5
aaronpicard <aaronpic19@gmail.com>
2024-10-09 20:12:23 -0400

    began refactoring EStop code to use stepper instead of servo

--- ESTOP_servomotor_oop.ino
+++ ESTOP_servomotor_oop.ino
@@ -5,59 +5,47 @@ Will either use button or switch (idea is to use a pushbutton switch to keep the
 We will change the servo motor out for the E-stop motor, and the redboard for the STM board
 */
 
-#include <Servo.h>
+#include <StepperMotorClass.ino>
+#include <String>
 
 class Estop {
   public: 
-    int switchPin;
-    int estopButton;
+    int estopComponent;
     int servoPin;
     int buttonPress = 0;
     String runMethod; //change this to "button" to use button
     StepperMotor motor;
 
     Estop(){
-      this->switchPin = 11;
-      this->estopButton = 10;
+      this->estopComponent = 10;
       this->motor = new StepperMotor(2, 3);
       this->runMethod = "switch";
-      pinMode(this->estopButton, INPUT_PULLUP);
-      pinMode(this->switchPin, INPUT_PULLUP);
+      pinMode(this->estopComponent, INPUT_PULLUP);
     }
-    Estop(int switch1, int button1, int dirPin, int stepPin, String run){
-      this->switchPin = switch1;
-      this->estopButton = button1;
+    Estop(int estop, int dirPin, int stepPin, String run){
+      this->estopComponent = estop;
       this->motor = new StepperMotor(dirPin, stepPin);
       this->runMethod = run;
     }
-    void checkEstopButton(){
-      // check if the button is being pressed
+
+    void checkEStopComponent() {
       int state;
-      if (digitalRead(estopButton) == LOW){
-        state = 1;
+      if (digitalRead(this->estopComponent) == LOW){
+        this->buttonPress = 1;
+        this->setAngleStepper(90);
       }
       else{
-        state = 0;
+        this->buttonPress = 0;
+        this->setAngleStepper(0);
       }
-      buttonPress = state;
-    }
-    void checkEstopSwitch(){
-      int state;
-      if (digitalRead(switchPin) == LOW){
-        state = 1;
-      }
-      else{
-        state = 0;
-      }
-      buttonPress = state;
     }
 
 void setAngleStepper(int angle) {
-  motor->setAngle(degree);
+  this->motor->setAngle(angle);
 }
 };
 
-Estop reNU_windmill(11,10,9, 2, 3); //create an object of Estop class
+Estop reNU_windmill(10, 9, 2, 3); //create an object of Estop class
 
 void setup() {
   // put your setup code here, to run once:
@@ -69,21 +57,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (reNU_windmill.runMethod == "button"){
-    reNU_windmill.checkEstopButton();
-  }
-  else if (reNU_windmill.runMethod == "switch") {
-    reNU_windmill.checkEstopSwitch();
-  }
-
-  if (reNU_windmill.buttonPress == 1){
-    reNU_windmill.myServo.write(90);
-    Serial.print("\nMoved the servo.");
-    delay(1000);
-  }
-  else{
-    reNU_windmill.myServo.write(0);
-    Serial.print("\nBack to original spot");
-    delay(1000);
-  }
+  reNU_windmill->checkEStopComponent();
 }
